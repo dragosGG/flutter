e2261026f742a1c4095d5195d0757c54f144be8f
Dragos <dragos.giugula@gmail.com>
2018-11-06 12:12:34 +0200

    moving into creating custom widgets

---
+++ lib/src/app.dart
@@ -0,0 +1,9 @@
+// import flutter helper library 
+
+import 'package:flutter/material.dart';
+// create a class that will be our custom widget
+// extend This class must extend the 'StatelessWidget' base class
+
+// must define a 'build' method that returns
+// the widgets that *this* widget will show 
+
